748a7e71507123a75b76f344d0119db24e95e211
asdf18909 <92042475+asdf18909@users.noreply.github.com>
2026-08-09 11:04:46 +0800

    update github pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   output: 'export',
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
+
+  trailingSlash: true,
+
+  assetPrefix: isProd ? 'https://airestart.indevs.in/' : undefined,
+
 }
 
 export default nextConfig
